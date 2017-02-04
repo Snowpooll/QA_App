b06a8a6f5677ed28c3813a36e7abc78452313bbd
snowpool <snowpoollovely@gmail.com>
2017-02-05 08:18:41 +0900

    drawer add

--- app/src/main/java/jp/techacademy/kubota/satoru/qa_app/MainActivity.java
+++ app/src/main/java/jp/techacademy/kubota/satoru/qa_app/MainActivity.java
@@ -2,8 +2,13 @@ package jp.techacademy.kubota.satoru.qa_app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -22,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -39,6 +44,38 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(intent);
                 }
 ;
+            }
+        });
+
+        //navigation drawer setting
+        DrawerLayout drawer = (DrawerLayout)findViewById(R.id.drawerlayout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,R.string.app_name,R.string.app_name);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView)findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                if(id == R.id.nav_hoby){
+                    toolbar.setTitle("趣味");
+                    mGenre=1;
+                }else if(id == R.id.nav_life){
+                    toolbar.setTitle("生活");
+                    mGenre=2;
+                }else if(id == R.id.nav_health){
+                    toolbar.setTitle("健康");
+                    mGenre=3;
+                }else if(id == R.id.nav_computer){
+                    toolbar.setTitle("コンピュータ");
+                    mGenre=4;
+                }
+
+                DrawerLayout drawer = (DrawerLayout)findViewById(R.id.drawerlayout);
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
             }
         });
     }
